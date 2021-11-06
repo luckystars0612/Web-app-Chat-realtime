313509d2ffa9211b9fec01e9b60047d25bbb38a3
Luckystars0612 <enormousmaster@gmail.com>
2021-11-07 01:06:32 +0700

    change home background

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("personal.urls")),
+    path('public-chat/',include("personal.urls")),
+    path('',views.homepage,name="home"),
     path('account/',include('account.urls')),
     path('friend/', include('friend.urls')),
     path('chat/',include('chat.urls')),
--- personal/urls.py
+++ personal/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.home_screen_view,name='home'),
+    path('',views.home_screen_view,name='public-chat'),
 ]
